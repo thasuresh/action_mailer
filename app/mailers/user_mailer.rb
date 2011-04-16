054efd8dd7a906906b2906f5b0797c3cb9662cbc
Tha.Suresh <suresh@mercury.labs.slashprog.com>
2011-04-16 17:58:35 +0530

    password image added

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,6 +3,7 @@
       
       def registration_confirmation(user)
       @user = user  
+      attachments["password.jpg"] = File.read("#{Rails.root}/public/images/password.jpg")
         mail(:to => user.email, :subject => "Registered")  
       end  
    end  
